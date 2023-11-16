12ffd20ecbcf1c90743fa25b89a3ae91263044b5
shivam <chaturvedishivam598@gmail.com>
2023-11-16 17:33:15 +0530

    Progress Bar Added

--- EncryptApp.py
+++ EncryptApp.py
@@ -1,8 +1,8 @@
 import tkinter as tk
-from PIL import Image,ImageTk
 from tkinter import filedialog,messagebox,simpledialog
+from tkinter.ttk import Progressbar
 import os
-import hashlib,base64
+import hashlib,base64,threading
 from cryptography.fernet import Fernet,InvalidToken
 
 class EncryptDecryptApp:
@@ -10,33 +10,28 @@ class EncryptDecryptApp:
         self.root=root
         self.active_button="e"
         self.icon_path="icon.ico"
-        self.image_path='key.jpg'
+        self.image_path='key.gif'
         root.title("  File Encrypter and Decrypter")
         try:
             root.iconbitmap(default=self.icon_path)
         except Exception as e:
             pass
-            # messagebox.showwarning(title="Icon",message=str(e))
         root.geometry("1000x600+0+0")
         root.configure(bg="#08CF9C")
         root.bind("<KeyPress>",self.on_key_press)
         try:
-            image=Image.open(self.image_path)
-            photo=ImageTk.PhotoImage(image)
+            photo=tk.PhotoImage(file=self.image_path)
             label=tk.Label(root,image=photo)
             label.pack(pady=(20,0),fill=tk.BOTH,padx=30)
         except Exception as e:
             pass
-            # messagebox.showwarning(title="Image",message=str(e))
 
         self.encrypt_button=tk.Button(root,text="ENCRYPT",bg="green",width=12,height=3,font=("Arial",15,"bold"),command=lambda:self.open_file_dialog("e"))
         self.decrypt_button=tk.Button(root,text="DECRYPT",bg="cyan",width=12,height=3,font=("Arial",15,"bold"),command=lambda:self.open_file_dialog("d"))
 
         self.encrypt_button.bind("<Enter>",self.on_enter)
-        self.encrypt_button.bind("<Leave>",self.on_leave)
         
         self.decrypt_button.bind("<Enter>",self.on_enter)
-        self.decrypt_button.bind("<Leave>",self.on_leave)
 
         self.encrypt_button.pack(pady=20,padx=30,fill=tk.BOTH)
         self.decrypt_button.pack(padx=30,fill=tk.BOTH)
@@ -56,7 +51,6 @@ class EncryptDecryptApp:
 
 
     def on_key_press(self,event):
-        # print(event)
         if(event.keycode==27):
             self.root.destroy()
         elif(event.keycode==38): #up arrow
@@ -76,12 +70,7 @@ class EncryptDecryptApp:
             self.active_button="d"
             self.encrypt_button.configure(bg="cyan")
             self.decrypt_button.configure(bg="green") 
-
-    
-    def on_leave(self,event):
-        button=event.widget
-        # button.configure(bg="cyan")
-        
+   
 
     
     def open_file_dialog(self,button):
@@ -99,7 +88,7 @@ class EncryptDecryptApp:
                         messagebox.showerror(title="Error",message="Please,Enter A Password!!")
                         
                     else:
-                        self.encrypt(file_path,password)
+                        threading.Thread(target=self.encrypt(file_path,password)).start()
                 else:
                     messagebox.showerror(title="Error",message="Please,Select A File !!")
                 
@@ -110,7 +99,7 @@ class EncryptDecryptApp:
                     if password is None:
                         messagebox.showerror(title="Error",message="Please,Enter A Password!!")
                     else:
-                        self.decrypt(file_path,password)
+                        threading.Thread(target=self.decrypt(file_path,password)).start()
                 else:
                     messagebox.showerror(title="Error",message="Please,Select A File !!")
         except Exception as e:
@@ -119,6 +108,9 @@ class EncryptDecryptApp:
 
     def encrypt(self,file_path,password):
         try:
+            progress_bar=Progressbar(self.root,length=200,mode="indeterminate")
+            progress_bar.place(x=500,y=300)
+            progress_bar.start(5)
             key=hashlib.sha256(password.encode("utf-8")).hexdigest()[:32]
             key=base64.b64encode(key.encode("utf-8"))
             cipher=Fernet(key)
@@ -129,14 +121,21 @@ class EncryptDecryptApp:
                 f.write(encrypted_data)
             os.system(f"attrib +R {file_path}.enc")
             os.remove(file_path)
-            
+            progress_bar.stop()
+            progress_bar.destroy()
             messagebox.showinfo(title="Success",message="File Encrypted Successfully !!")
-
+          
         except Exception as e:
+            progress_bar.stop()
+            progress_bar.destroy()
             messagebox.showerror(title="Error",message=str(e))
+       
 
     def decrypt(self,file_path,password):
         try:
+            progress_bar=Progressbar(self.root,length=200,mode="indeterminate")
+            progress_bar.place(x=500,y=300)
+            progress_bar.start(5)
             key=hashlib.sha256(password.encode("utf-8")).hexdigest()[:32]
             key=base64.b64encode(key.encode("utf-8"))
             
@@ -149,11 +148,17 @@ class EncryptDecryptApp:
             with open(file_path[:-4],"wb") as f:
                 f.write(decrypted_data)
             os.remove(file_path)
+            progress_bar.stop()
+            progress_bar.destroy()
             messagebox.showinfo(title="Success",message="File Decrypted Successfully !!")
         
         except InvalidToken as e:
+            progress_bar.stop()
+            progress_bar.destroy()
             messagebox.showerror(title="Error",message="Wrong Password !!")
         except Exception as e:
+            progress_bar.stop()
+            progress_bar.destroy()
             messagebox.showerror(title="Error",message=str(e))
 
 
